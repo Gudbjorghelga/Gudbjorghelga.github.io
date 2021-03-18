0d0d45facb0d68ce7fcf400bfd50f5c434b6a4f7
Gudbjorghelga <gudbjorghelgae@gmail.com>
2021-03-18 11:56:51 +0000

    up

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -1,1 +1,11 @@
 // Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(500,500);
+    background(255,0,255);
+    fill(150);
+    ellipse(100,100,50,50);
+    ellipse(200,200,50,50);
+    fill(100);
+    textSize(20);
+    text("Halló heimur",200,40);
+   }
